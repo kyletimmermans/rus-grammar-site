ced781a494282e88b58b549503a67ae6d81a6fd8
Kyle Timmermans <47165102+kyletimmermans@users.noreply.github.com>
2023-08-11 22:21:41 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -1,1 +1,39 @@
-console.log("Hello, world!");
+const verb = () => {
+    console.log("verb");
+};
+
+const singNoun = () => {
+    console.log("Hello, world!");
+};
+
+const plurNoun = () => {
+    console.log("Hello, world!");
+};
+
+const singAdj = () => {
+    console.log("Hello, world!");
+};
+
+const plurAdj = () => {
+    console.log("Hello, world!");
+};
+
+const pronoun = () => {
+    console.log("Hello, world!");
+};
+
+const possesive = () => {
+    console.log("Hello, world!");
+};
+
+const demonstrative = () => {
+    console.log("Hello, world!");
+};
+
+const comparative = () => {
+    console.log("Hello, world!");
+};
+
+const about = () => {
+    console.log("Hello, world!");
+};
